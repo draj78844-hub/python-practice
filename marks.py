f3ca4dcadab56fe3f8faeb6b6bda550010401dcd
draj78844-hub <draj78844@gmail.com>
2026-07-13 14:51:22 +0530

    marks grade

--- marks.py
+++ marks.py
@@ -1,7 +1,7 @@
 marks = int(input("enter your marks: "))
 
 if(marks<=100 and marks>=90):
-    grade = "EX"
+    grade = "EXCELLENT"
 elif(marks<90 and marks>=80):
     grade = "A"
 elif(marks<80 and marks>=70):
