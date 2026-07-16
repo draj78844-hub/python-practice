ecf9a67226f49fdc847e2ec2f42f2e956dba31e0
draj78844-hub <draj78844@gmail.com>
2026-07-16 17:06:36 +0530

    number square

--- square.py
+++ square.py
@@ -1,10 +1,8 @@
-#a = int(input("enter your number: "))
-
-#print("the square of the number is", a**2)
-#print("the square of the number is", a*a)
+a = int(input("enter your number: "))
+print("the square of the number is", a*a)
 
 
 
 
-square=lambda x:x*x
-print(square(6))
+# square=lambda x:x*x
+# print(square(6))
