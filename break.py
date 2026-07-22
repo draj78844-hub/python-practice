87fc9e93e2795cde619ee1585d64d7ca6967209a
draj78844-hub <draj78844@gmail.com>
2026-07-22 13:57:12 +0530

    break loop

--- break.py
+++ break.py
@@ -1,5 +1,22 @@
-#using break loop:
-for i in range(1, 11):
-    if i == 7:
-        break
-    print(i)
+# #using break loop:
+# for i in range(1, 11):
+#     if i == 7:
+#         break
+#     print(i)
+
+ 
+
+def break_loop(i):
+
+    try:
+        for i in range(1, 11):
+            if i == 7:
+                break
+
+            print(i)
+
+    except Exception as e:
+        print("Error: ",e)
+
+if __name__== "__main__":
+    break_loop(6)
