f0f83665f08b4bcc6e44385e98bd5e796c9d7747
draj78844-hub <draj78844@gmail.com>
2026-07-23 15:17:36 +0530

    name list

--- namelist.py
+++ namelist.py
@@ -1,9 +1,25 @@
-l = ["prince", "deep", "ladoo", "pugu"]
-name = input("enter your name: ")
+# l = ["prince", "deep", "ladoo", "pugu"]
+# name = input("enter your name: ")
 
-if(name in l):
-    print("your name is in the list")
+# if(name in l):
+#     print("your name is in the list")
 
-else:
-    print("your name is not in the list")
-    
+# else:
+#     print("your name is not in the list")
+
+#WAP to name list
+def entered_list(l):
+
+    try:
+        name= input("enter your name: ")
+        if(name in l):
+            print("your name is in the list")
+
+        else:
+            print("your name is not in the list")
+
+    except Exception as e:
+        print("Error: ",e)
+
+if __name__ == "__main__":
+    entered_list("prince, deep, ladoo, pugu")
