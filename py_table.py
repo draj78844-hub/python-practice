28b4e7628e351cc547e241b1b40879b66a0b99b8
draj78844-hub <draj78844@gmail.com>
2026-07-23 16:20:44 +0530

    table

--- py_table.py
+++ py_table.py
@@ -1,6 +1,15 @@
-#print a table
-n = int(input("enter number:  "))
-i = 1
-while i <= 10:
-    print(n * i)
-    i += 1
+#WAP to table
+def print_table(i):
+
+    try:
+        i= 1
+        n= int(input("enter number: "))
+        while i <= 10:
+            print(n*i)
+            i += 1
+
+    except Exception as e:
+        print("Error: ",e)
+
+if __name__ == "__main__":
+    print_table(8)
