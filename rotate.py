a4ddcc5eabf1421e1eccf853ec695cd9109be794
draj78844-hub <draj78844@gmail.com>
2026-07-23 16:53:53 +0530

    rotate list

--- rotate.py
+++ rotate.py
@@ -1,5 +1,18 @@
-#rotate list
-lst = [1,2,3,4,5]
-k = 2
+# #rotate list
+# lst = [1,2,3,4,5]
+# k = 2
 
-print(lst[k:] + lst[:k])
+# print(lst[k:] + lst[:k])
+
+# #WAP to rotate list
+def rotate_list(l):
+
+    try:
+        k = 2
+        print(l[k:] + l[:k])
+
+    except Exception as e:
+        print("Error: ",e)
+
+if __name__ == "__main__":
+    rotate_list([1,2,3,4,5])
