22a83c64402929bac1347e640acd290c86b6f4ea
draj78844-hub <draj78844@gmail.com>
2026-07-23 16:11:10 +0530

    print counting

--- num.py
+++ num.py
@@ -1,5 +1,19 @@
 #WAP to print 1 to 100 num:
-i = 100
-while i >= 1:   #this is a stopping value
-    print(i)
-    i -= 1
+# i = 100
+# while i >= 1:   #this is a stopping value
+#     print(i)
+#     i -= 1
+
+#WAP to print 1 to 100 num:
+def counting_num(i):
+
+    try:
+        while i >= 1:
+            print(i)
+            i -= 1
+
+    except Exception as e:
+        print("Error: ",e)
+
+if __name__ == "__main__":
+    counting_num(100)
