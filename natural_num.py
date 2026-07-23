7217f61abb9467a8f37912fd2a4d0281b6430027
draj78844-hub <draj78844@gmail.com>
2026-07-23 15:55:30 +0530

    natural number

--- natural_num.py
+++ natural_num.py
@@ -1,5 +1,21 @@
-n = int(input("enter a number: "))
-i = 1
-while i <= n:
-    print(i)
-    i += 2
+# n = int(input("enter a number: "))
+# i = 1
+# while i <= n:
+#     print(i)
+#     i += 2
+
+#WAP to natural number
+def natural_num(i):
+
+    try:
+        n= int(input("enter a number: "))
+        i = 1
+        while i <= n:
+            print(i)
+            i += 2
+
+    except Exception as e:
+        print("Error: ")
+
+if __name__ == "__main__":
+    natural_num(7)
