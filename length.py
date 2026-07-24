0683d577f47dacf118d48822df00997aac49cad1
draj78844-hub <draj78844@gmail.com>
2026-07-24 12:56:49 +0530

    len username

--- length.py
+++ length.py
@@ -1,7 +1,25 @@
-username = input("enter username: ")
+# username = input("enter username: ")
 
-if(len(username)<10):
-    print("your username contain less than 10 character")
+# if(len(username)<10):
+#     print("your username contain less than 10 character")
 
-else:
-    print("all is well")
+# else:
+#     print("all is well")
+
+#WAP to len of username
+def len_username(s):
+
+    try:
+        username= input("Enter username: ")
+
+        if(len(username)<10):
+            print("your username contain less than 10 character")
+
+        else:
+            print("All is well")
+
+    except Exception as e:
+        print("Error: ",e)
+
+if __name__ == "__main__":
+    len_username("prince raj")
