58c211e18b76c23869aaf5f530a0694356f1dbee
draj78844-hub <draj78844@gmail.com>
2026-07-25 13:24:34 +0530

    name different  types

--- string.py
+++ string.py
@@ -1,6 +1,23 @@
-name = "prince"
+# name = "prince"
 
-print(len(name))
-print(name.endswith("ce"))
-print(name.startswith("pri"))
-print(name.capitalize())
+# print(len(name))
+# print(name.endswith("ce"))
+# print(name.startswith("pri"))
+# print(name.capitalize())
+
+
+#WAP to name different types pattern
+def string_operation():
+    try:
+        name= "prince"
+
+        print(len(name))
+        print(name.endswith("ce"))
+        print(name.startswith("ra"))
+        print(name.capitalize())
+
+    except Exception as e:
+        print("Error: ",e)
+
+if __name__ == "__main__":
+    string_operation()
