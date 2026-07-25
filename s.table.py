581c4c3da647829e880fa60f449418f8e8b7bcb9
draj78844-hub <draj78844@gmail.com>
2026-07-25 13:03:18 +0530

    multiple sign use in table

--- s.table.py
+++ s.table.py
@@ -1,7 +1,7 @@
 #WAP to print table:
 
 n = int(input("enter number: "))
-i = 
+i = 65
 while i <= 10:
     print(n * i)
     i += 1
--- table.py
+++ table.py
@@ -1,5 +1,20 @@
-n = int(input("enter a number: "))
+# n = int(input("enter a number: "))
 
-for i in range(1, 11):
+# for i in range(1, 11):
     
-     print(f"{n} * {i} = {n * i}")
+#      print(f"{n} * {i} = {n * i}")
+
+
+#WAP to multiple sign uses in table.
+def multiple_signtable(i):
+     n= int(input("enter a number: "))
+
+     try:
+          for i in range(1, 11):
+               print(f"{n} * {i} = {n * i}")
+
+     except Exception as e:
+          print("Error: ,e")
+
+if __name__ == "__main__":
+     multiple_signtable(5)
