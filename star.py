1db5109620b074ee621e634153089382987d1c4b
draj78844-hub <draj78844@gmail.com>
2026-07-28 17:36:52 +0530

    pyramid

--- star.py
+++ star.py
@@ -1,5 +1,21 @@
-n = int(input("Enter a number: "))
+# n = int(input("Enter a number: "))
 
-for i in range(1, n + 1):
-    print(" " * (n - i), end="")
-    print("*" * (2 * i - 1))
+# for i in range(1, n + 1):
+#     print(" " * (n - i), end="")
+#     print("*" * (2 * i - 1))
+
+
+#WAP to pyramid.
+def pyramid(n):
+
+    try:
+        n= int(input("Enter a number: "))
+
+        for i in range(1, n+1):
+            print(" " * (n - i),end= "")
+            print("*" *  (2 * i - 1))
+
+    except Exception as e:
+        print("Error: ",e)
+
+pyramid(4)
