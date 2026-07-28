7237c9ef9ae60d6e2dcace4777ecb16815be58c3
draj78844-hub <draj78844@gmail.com>
2026-07-28 17:52:45 +0530

    using loop

--- using.py
+++ using.py
@@ -1,8 +1,27 @@
-#WAP to list using a loop
-nums = [88, 98, 76, 95, 67, 78]
-god = ["hanuman", "sankar", "maa", "bholenath"]
+# #WAP to list using a loop
+# nums = [88, 98, 76, 95, 67, 78]
+# god = ["hanuman", "sankar", "maa", "bholenath"]
 
-i = 0
-while i < len(god):
-    print(god[i])
-    i += 1
+# i = 0
+# while i < len(god):
+#     print(god[i])
+#     i += 1
+
+
+#WAP to using loop
+def using_loop(s):
+
+    try:
+        
+        god= s.split(", ")
+
+        i = 0
+        while i < len(god):
+            print(god[i])
+            i += 1
+
+    except Exception as e:
+        print("Error: ")
+
+if __name__ == "__main__":
+    using_loop("hanuman, sankar, god")
