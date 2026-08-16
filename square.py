6536ceb97bbb0ea039b17bfd29d5754e0ad20108
draj78844-hub <draj78844@gmail.com>
2026-08-16 16:10:38 +0530

    square number

--- square.py
+++ square.py
@@ -1,8 +1,19 @@
-a = int(input("enter your number: "))
-print("the square of the number is", a*a)
-
-
-
+# a = int(input("enter your number: "))
+# print("the square of the number is", a*a)
 
 # square=lambda x:x*x
-# print(square(6))
+
+def square_number(i):
+
+    try:
+        a= int(input("enter your number: "))
+        print("the square of the number is", a*a)
+        square= lambda x:x*x
+
+    except  Exception as e:
+        print("Error: ",e)
+
+
+if __name__ == "__main__":
+    square_number(5)
+
