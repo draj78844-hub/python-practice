e9c7b21d09a410d7b9a0c54c5c10e89c9a0a8b1b
draj78844-hub <draj78844@gmail.com>
2026-08-16 16:31:20 +0530

    letter number

--- st_ring.py
+++ st_ring.py
@@ -1,2 +1,14 @@
-str = "i am studying from python"
-print(str.find("p"))
+# str = "i am studying from python"
+# print(str.find("p"))
+
+def letter_number():
+
+    try:
+        str= "i am studying from python"
+        print(str.find("i"))
+
+    except Exception as e:
+        print("Error: ",e)
+
+if __name__ == "__main__":
+    letter_number()
