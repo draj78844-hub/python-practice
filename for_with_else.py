85f93b12af8f27869fdc08dfe00e4db75a7c3d65
draj78844-hub <draj78844@gmail.com>
2026-08-31 13:55:15 +0530

    for with else

--- for_with_else.py
+++ for_with_else.py
@@ -1,13 +1,39 @@
-l = [1, 5, 6, 8]
-for item in l:
-    print(item)
+# l = [1, 5, 6, 8]
+# for item in l:
+#     print(item)
 
-else:
-    print("done")
+# else:
+#     print("done")
 
 
-for i in range(101):
-    if(i == 101):
-        #break
-        continue
-    print(i)
+# for i in range(101):
+#     if(i == 101):
+#         #break
+#         continue
+#     print(i)
+
+
+
+
+#WAP to for with else function
+def for_with_else(i):
+
+    try:
+        l = [1,3,2,5,9,8]
+        for item in l:
+            print(item)
+
+        else:
+            print("done")
+
+        for i in range(101):
+            if(i == 101):
+                #break
+                continue
+            print(i)
+
+    except Exception as e:
+        print("Error: ",e)
+
+if __name__ == "__main__":
+    for_with_else(101)
