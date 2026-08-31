28606927bcc8fce780cacf551d8eb17982a6db1e
draj78844-hub <draj78844@gmail.com>
2026-08-31 14:23:54 +0530

    inheritance code

--- inheritance.py
+++ inheritance.py
@@ -1,15 +1,42 @@
-class A:
-    varA = "welcome to class A"
+# class A:
+#     varA = "welcome to class A"
 
-class B:
-    varB = "welcome to class B"
+# class B:
+#     varB = "welcome to class B"
 
-class C(A,B):
-    varC = "welcome to class C"
+# class C(A,B):
+#     varC = "welcome to class C"
 
-c1 = C()
+# c1 = C()
 
-print(c1.varA)
-print(c1.varB)
-print(c1.varC)
+# print(c1.varA)
+# print(c1.varB)
+# print(c1.varC)
 
+
+#WAP to inheritance
+def inheritance():
+
+    try:
+        class A:
+            varA = "Welcome to class A prince babu"
+
+        class B:
+            varB = "Welcome to class B b tech course"
+
+        class C(A, B):
+            varC = "Welcome to class C welcome in my house"
+
+        c1 = C()
+
+        print(c1.varA)
+        print(c1.varB)
+        print(c1.varC)
+
+    except Exception as e:
+        print("Error: ", e)
+
+if __name__ == "__main__":
+    inheritance()
+
+         
