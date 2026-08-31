5205e8dd5c056b757ee9f488c86c0a6b03556d2e
draj78844-hub <draj78844@gmail.com>
2026-08-31 13:23:45 +0530

    list method

--- def.py
+++ def.py
@@ -1,26 +1,42 @@
-#def show(n):
-   # if(n == 0):
-   #     return 
-   # print(n)
-  #  show(n -  1)
+# #def show(n):
+#    # if(n == 0):
+#    #     return 
+#    # print(n)
+#   #  show(n -  1)
 
-#show(10)
-
-
-#def fact(n):
- #   if(n == 1 or n == 0):
- #       return 1
- #   return fact(n - 1) * n
-
-#print(fact(6))
+# #show(10)
 
 
-def print_list(list, idx=0):
-    if (idx == len(list)):
-        return
-    print(list[idx])
-    print_list(list, idx+1)
+# #def fact(n):
+#  #   if(n == 1 or n == 0):
+#  #       return 1
+#  #   return fact(n - 1) * n
+
+# #print(fact(6))
 
 
-fruits =["mango", "papaya", "orange", "black berry", "guava", "litchi"]
-print_list(fruits)
+# def print_list(list, idx=0):
+#     if (idx == len(list)):
+#         return
+#     print(list[idx])
+#     print_list(list, idx+1)
+
+
+# fruits =["mango", "papaya", "orange", "black berry", "guava", "litchi"]
+# print_list(fruits)
+
+
+def print_list(items, idx=0):
+
+    try:
+        if idx == len(items):
+            return
+        
+        print(items[idx])
+        print_list(items, idx + 1)
+
+    except Exception as e:
+        print("Error: ,e")
+
+if __name__ == "__main__":
+    print_list(["mango", "papaya", "coconut", "kiwi", "guava", "apple"])
--- listmethod.py
+++ listmethod.py
@@ -1,7 +1,23 @@
-l1 = [1, 54, 76,32,23,12,89,90]
-#l1.sort()
-#l1.reverse()
-#l1.insert(3,333)
-value = l1.pop(3)
-print(value)
-print(l1)
+# l1 = [1, 54, 76,32,23,12,89,90]
+# #l1.sort()
+# #l1.reverse()
+# #l1.insert(3,333)
+# value = l1.pop(3)
+# print(value)
+# print(l1)
+
+
+
+#WAP to list method in reverse terms.
+def list_method(i):
+
+    try:
+        l1= [23, 45, 34, 65, 44, 33]
+        l1.reverse()
+        print(l1)
+
+    except Exception as e:
+        print("Error: , e")
+
+if __name__ == "__main__":
+    list_method(4)
