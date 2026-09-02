d8442c91ae037e8a8fdfe6faf5e54b3103c57f33
draj78844-hub <draj78844@gmail.com>
2026-09-02 16:14:42 +0530

    fibonacci

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,9 +1,30 @@
+# #WAP to fibonacci series
+# n = int(input("enter number of terms: "))
+# a = 0
+# b = 1
+# for i in range(n):
+#     print(a, end=" ")
+#     c = a+b
+#     a = b
+#     b = c 
+
+
 #WAP to fibonacci series
-n = int(input("enter number of terms: "))
-a = 0
-b = 1
-for i in range(n):
-    print(a, end=" ")
-    c = a+b
-    a = b
-    b = c 
+def fibonacci_series(i):
+
+    try:
+        n= int(input("enter number of terms: "))
+        a= 0
+        b= 1
+
+        for i in range(n):
+            print(a, end= " ")
+            c= a+b
+            a= b
+            b= c
+
+    except Exception as e:
+        print("Error: ", e)
+
+if __name__ == "__main__":
+    fibonacci_series(6)
