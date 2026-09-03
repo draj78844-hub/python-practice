9bfdd7923d206bbd89156d915e4fc993f7508e4e
draj78844-hub <draj78844@gmail.com>
2026-09-03 16:38:29 +0530

    set

--- set.py
+++ set.py
@@ -7,7 +7,24 @@
 
 
 
-i = 1
-while i <= 10:
-    print(5 * i)
-    i += 1
+# i = 1
+# while i <= 10:
+#     print(5 * i)
+#     i += 1
+
+
+#WAP to set and multiplication
+def set(i):
+
+    try:
+
+        i= 1
+        while i<= 10:
+            print(5 * i)
+            i += 1
+
+    except Exception as e:
+        print("Error: ", e)
+
+if __name__ == "__main__":
+    set(5)
