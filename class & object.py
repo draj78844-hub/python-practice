85e1e353c184cb70cd5e361b1eb57e7f8ab46da4
draj78844-hub <draj78844@gmail.com>
2026-09-12 19:41:00 +0530

    import random

--- class & object.py
+++ class & object.py
@@ -105,14 +105,14 @@
 
 
 
-sum= 0
-while(True):
-    userInput= input("Enter the item price or press q to quit: \n")
-    if (userInput != 'q'):
-        sum= sum + int(userInput)
-        print(f"order total so far: {sum}")
+# sum= 0
+# while(True):
+#     userInput= input("Enter the item price or press q to quit: \n")
+#     if (userInput != 'q'):
+#         sum= sum + int(userInput)
+#         print(f"order total so far: {sum}")
 
-    else:
-        print(f"your bill total is {sum}. Thanks for shopping with us")
-        break
+#     else:
+#         print(f"your bill total is {sum}. Thanks for shopping with us")
+#         break
 
---
+++ import random.py
@@ -0,0 +1,7 @@
+#WAP to print import random 
+import random
+when= ["Few years ago", "yesterday", "Today"]
+where=["at mumbai", "at delhi", "at bhopal"]
+who= ["prince", "rohit", "raushan"]
+what= ["marks in 397 in intermediate", "marks in 332 in 9th class", "marks in 432 in 8th class"]
+print(random.choice(when)+" "+random.choice(where)+" "+random.choice(who)+" "+random.choice(what))
