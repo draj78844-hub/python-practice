aa03db6c483ea079e73ee9988ede32639cedf426
draj78844-hub <draj78844@gmail.com>
2026-09-17 20:59:19 +0530

    store in movies name

--- calculator.py
+++ calculator.py
@@ -91,16 +91,18 @@
 # tup= (3,3,2,3,4,5,5,7,8,6,5)
 # print(tup.count(7))
 
-# movies= []
-# mov1= input("enter 1st movie: ")
-# mov2= input("enter 2nd movie: ")
-# mov3= input("enter 3rd movie: ")
 
-# movies.append(mov1)
-# movies.append(mov2)
-# movies.append(mov3)
+# WAP to store 3 movies in a list
+movies= []
+mov1= input("enter 1st movie: ")
+mov2= input("enter 2nd movie: ")
+mov3= input("enter 3rd movie: ")
 
-# print(movies)
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+
+print(movies)
 
 
 
